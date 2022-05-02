b1b6b5f4b672746e6bd07817939ff9247733126a
Carlos-Owens <clojr12@gmail.com>
2022-05-01 20:14:33 -0400

    Add operator function
    This function will determine the equation with a switch case and call the other four functions.

--- script.js
+++ script.js
@@ -14,4 +14,21 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-//console.log(multiply(5,2));
+function operator (operator, num1, num2 ) {
+    let results = 0;
+    switch (operator) {
+        case "+":
+           results = add(num1, num2);
+            break;
+        case "-":
+           results =  subtract(num1, num2);
+            break;
+        case "*":
+           results =  multiply(num1, num2);
+            break;
+        case "/":
+           results =  divide(num1, num2);
+            break;
+    }
+    return results;
+}
