c17265dc07d8244999a3bb3915f8ef70318dfb8a
Carlos-Owens <clojr12@gmail.com>
2022-05-04 18:58:09 -0400

    Output results on the display rounded

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
+let num1 = 0;
+let num2 = 0;
+let tempNum1 = [];
+let tempNum2 = [];
+let firstValue = 0;
+let tempOperator = [];
+//let operator = "";
+
 function add(num1, num2) {
-    return num1 + num2
+    let results = (num1 + num2);
+    let rounded = Math.round((results + Number.EPSILON) * 100) / 100;
+    rounded =  rounded.toFixed(2);
+    const calcDisplay = document.querySelector("#display");
+    calcDisplay.textContent = rounded;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    let results = (num1 / num2);
+    if (results === "infinity") {
+        return "Error!";
+    } else {
+    let rounded = Math.round((results + Number.EPSILON) * 100) / 100;
+    rounded =  rounded.toFixed(2);
+    const calcDisplay = document.querySelector("#display");
+    calcDisplay.textContent = rounded;
+    } 
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
-}
+    let results = (num1 - num2);
+    let rounded = Math.round((results + Number.EPSILON) * 100) / 100;
+    rounded =  rounded.toFixed(2);
+    const calcDisplay = document.querySelector("#display");
+    calcDisplay.textContent = rounded;}
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    let results = (num1 * num2);
+    let rounded = Math.round((results + Number.EPSILON) * 100) / 100;
+    rounded =  rounded.toFixed(2);
+    const calcDisplay = document.querySelector("#display");
+    calcDisplay.textContent = rounded;
 }
 
 function operator (operator, num1, num2 ) {
-    let results = 0;
+    
     switch (operator) {
         case "+":
-           results = add(num1, num2);
+           add(num1, num2);
             break;
         case "-":
-           results =  subtract(num1, num2);
+            subtract(num1, num2);
             break;
         case "*":
-           results =  multiply(num1, num2);
+           multiply(num1, num2);
             break;
         case "/":
-           results =  divide(num1, num2);
+           divide(num1, num2);
             break;
     }
-    return results;
 }
